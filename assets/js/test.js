407640bb948c0df359a4c4f4a1b7bda1b93b383d
RousselRojas <rousell.tfe@gmail.com>
2022-01-20 21:29:20 +0800

    init

---
+++ assets/js/test.js
@@ -0,0 +1,7 @@
+const myHeading = document.querySelector('h2');
+myHeading.textContent = 'Welcome to my webpage!';
+
+function greetMe(){
+    alert("Hello" + greetMe)
+}
+console.log(eval('3+5')); 
