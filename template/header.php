fb7acf5b06a3259a87022387bffa81ad41b6476b
RousselRojas <rousell.tfe@gmail.com>
2022-01-27 05:06:17 +0800

    ....

---
+++ template/header.php
@@ -0,0 +1,15 @@
+<?php 
+
+?>
+
+<header>
+    <h2>This is the header</h2>
+    <nav>
+        <ul>
+            <li><a href="<javascript::void>">Home</a></li>
+            <li><a href="<javascript::void>">About</a></li>
+            <li><a href="<javascript::void>">Projects</a></li>
+            <li><a href="<javascript::void>">Contact</a></li>
+        </ul>
+    </nav>
+</header>
